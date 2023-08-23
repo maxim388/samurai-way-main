f5fe26f7d4bead83bfcf0f7af3c4a013ca6ddae3
Maxim Alenchikov <maxim.alenchikov@gmail.com>
2023-08-23 19:35:26 +0300

    samurai-way v.056

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -10,7 +10,9 @@ import {
   setUsersAC,
   unfollowAC,
 } from "../../redux/users-reducer";
-import { UsersAPIComponent } from "./UsersAPIComponent";
+import axios from "axios";
+import React from "react";
+import { Users } from "./Users";
 
 type MapStatePropsType = UsersPageType;
 
@@ -22,7 +24,8 @@ type MapDispatchToPropsType = {
   setTotalUsersCount: (totalCount: number) => void;
 };
 
-export type UsersContainerPropsType = MapStatePropsType & MapDispatchToPropsType;
+export type UsersContainerPropsType = MapStatePropsType &
+  MapDispatchToPropsType;
 
 const mapStateToProps = (state: StateType): MapStatePropsType => {
   return {
@@ -52,6 +55,49 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
     },
   };
 };
+
+export class UsersAPIComponent extends React.Component<UsersContainerPropsType> {
+  componentDidMount = (): void => {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  };
+
+  onPageChanged = (pageNumber: number) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+  render() {
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+    return (
+      <Users
+        pages={pages}
+        currentPage={this.props.currentPage}
+        onPageChanged={this.onPageChanged}
+        users={this.props.users}
+        unfollow={this.props.unfollow}
+        follow={this.props.follow}
+      />
+    );
+  }
+}
 
 export const UsersContainer = connect(
   mapStateToProps,
