fb2e91ae1c267b92fcb6a0d9dd8162f745968aab
Maxim Alenchikov <maxim.alenchikov@gmail.com>
2023-08-30 21:10:20 +0300

    samurai-way v.075

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -17,7 +17,7 @@ const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
         <Field placeholder={"Password"} component={"input"} name={"password"} />
       </div>
       <div>
-        <Field type="checkbox" component={"input"} name={"remember me"} />
+        <Field type="checkbox" component={"input"} name={"rememberMe"} />
         rememver me
       </div>
       <div>
